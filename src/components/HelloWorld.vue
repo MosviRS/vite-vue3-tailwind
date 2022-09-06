beb9a24d16d3041cb6dbff6c754d58277f5776d0
MosviRS <sibace_sa@hotmail.com>
2022-09-05 20:26:32 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, computed, defineProps } from 'vue';
+
+const props = defineProps({
+  color: String,
+});
+
+const tc = computed(() => `bg-[${props.color}]`);
+</script>
+
+<template>
+  <div class="w-[100px] h-[100px]">
+    {{ tc }}
+  </div>
+</template>
+
+<style scoped></style>
